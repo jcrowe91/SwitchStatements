ecb1e03e4ae926f7957e55a7d75035646fddb3b3
jcrowe91 <jcrowe9119@gmail.com>
2021-08-07 22:04:07 -0500

    Add project files.

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite subject?");
+            string answer = Console.ReadLine();
+
+            switch (answer) 
+            {
+                case "Math":
+                    Console.WriteLine("Math is great!");
+                    break;
+                case "English":
+                    Console.WriteLine("English is so useful!");
+                    break;
+                case "History":
+                    Console.WriteLine("Thats my favorite too!");
+                    break;
+                case "Science":
+                    Console.WriteLine("Blinding me with science!");
+                    break;
+                case "Spanish":
+                    Console.WriteLine("Eso es impresionante!");
+                    break;
+                default:
+                    Console.WriteLine("Never heard of that one!");
+                    break;
+
+            }
+
+        }
+    }
+}
